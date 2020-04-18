faea09454b11391cbf4db028a3cb312e262cfbfb
idith almog <almog.idith@gmail.com>
2020-04-18 18:51:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,13 +28,13 @@ def position_taken?(board,index)
   end
 end
 def turn(board)
+  until valid_move?(board, index)
   puts "Please enter 1-9:"
   input=gets.strip
   index=input_to_index(input)
-  if valid_move?(board, index)
+  end
   move(board, index, "X")
-else
-  puts "Please enter 1-9:"
+
   end
 
 end
